54fd6348e6874280a02a1eba7ddb770442bea9df
Dmitry Korotkov <korotkov.dmitriy10.1@gmail.com>
2020-04-17 23:10:34 +0400

    task of text-editor complete

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,15 @@
+let editor = document.getElementById("editor");
+editor.insertAdjacentHTML("afterend",`<button id="clearText" style = "display:block">Очистить содержимое</button>`);
+let clearButton = document.getElementById("clearText");
+editor.addEventListener("input",(e)=> {
+    localStorage.editorValue = editor.value;   
+});
+
+clearButton.addEventListener("click",(e)=> {
+    editor.value = "";
+    localStorage.editorValue = editor.value;
+});
+
+window.onload = () => {
+    editor.value = localStorage.editorValue;
+}
